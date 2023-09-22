6ba22ae80edc468c0e82efecf2c12a335a08fbff
LWardman <lewis2304@hotmail.co.uk>
2023-09-22 11:07:15 +0200

    Moved string helper function out of main into it's own class.

--- stringFuncs.cpp
+++ stringFuncs.cpp
@@ -1,5 +1,87 @@
-//
-// Created by lewis on 22/09/2023.
-//
 
 #include "stringFuncs.h"
+
+#include <string>
+#include <algorithm>
+
+using std::string;
+
+string stringFuncs::reverseString(const string& txt)
+{
+    string reversed(txt.size(), ' ');
+
+    std::ranges::reverse_copy(txt.begin(), txt.end(), reversed.begin());
+
+    return reversed;
+}
+
+bool stringFuncs::isPalindrome(const string& s)
+{
+    string inputCopy = s;
+
+    std::ranges::remove_if(inputCopy.begin(), inputCopy.end(), ispunct); // Cleans up punctuation
+    std::ranges::remove_if(inputCopy.begin(), inputCopy.end(), isspace); // Cleans up whitespace
+
+    std::transform(inputCopy.begin(), inputCopy.end(), inputCopy.begin(),
+                   [](unsigned char c){return std::tolower(c);});
+
+    return inputCopy == reverseString(inputCopy);
+}
+
+string stringFuncs::rotateString(const string& txt)
+{
+    string inputCopy = txt;
+
+    std::transform(inputCopy.begin(), inputCopy.end(), inputCopy.begin(),
+                   [](unsigned char c){c++; if (c == '{') c = 'a'; return c;});
+
+    return inputCopy;
+}
+
+std::vector<string> stringFuncs::separateStringIntoWords(const string& txt)
+{
+    std::vector<string> words;
+
+    int start = 0;
+    int end = 0;
+
+    char space = ' ';
+
+    while ((start = txt.find_first_not_of(space, end)) != string::npos)
+    {
+        end = txt.find(space, start);
+
+        words.push_back(txt.substr(start, end - start));
+    }
+
+    return words;
+}
+
+string stringFuncs::capitalizeString(const string& txt)
+{
+    std::vector<string> splitString = separateStringIntoWords(txt);
+
+    string capitalizedString;
+
+    for (string word : splitString)
+    {
+        word[0] = std::toupper(word[0]);
+        capitalizedString += word;
+        capitalizedString += ' ';
+    }
+
+    capitalizedString.erase(capitalizedString.end() - 1, capitalizedString.end());
+
+    return capitalizedString;
+}
+
+string stringFuncs::largestWordInString(const string& txt)
+{
+    std::vector<string> words = separateStringIntoWords(txt);
+
+    std::ranges::sort(words.begin(), words.end(),
+                      [](const string& lhs, const string& rhs)
+                      {return lhs.size() > rhs.size();});
+
+    return words[0];
+}
--- stringFuncs.h
+++ stringFuncs.h
@@ -1,14 +1,59 @@
-//
-// Created by lewis on 22/09/2023.
-//
 
 #ifndef STRINGS_STRINGFUNCS_H
 #define STRINGS_STRINGFUNCS_H
 
+#include <string>
+#include <vector>
+
+using std::string;
 
 class stringFuncs {
 
-};
+public:
 
+    static string reverseString(const string& txt);
+
+    static bool isPalindrome(const string& s);
+
+
+    /**
+    * * Rotating a string means that each letter in the string gets sent to the next letter in the alphabet.
+    *
+    *  * For example. "abc" returns "bcd", "maze" returns "nbaf". Importantly, "z" rotates to "a".
+    *
+    * @param txt The string to be rotated.
+    * @returns The rotated string.
+    */
+    static string rotateString(const string& txt);
+
+    /**
+    *  e.g "My name is" returns {"My", "name", "is"}.
+    *
+    * @param txt The (ideally multiple worded) string.
+    * @return a vector of each individual word in the string.
+    */
+    static std::vector<string> separateStringIntoWords(const string& txt);
+
+    /**
+    * * This function capitalizes a string, which can be useful to transform a string into a more formal string.
+    *
+    * * Capitalizes ALL words in the sentence.
+    *
+    * * e.g "hello there" -> "Hello There"
+    *
+    * @param txt The sentence to capitalize.
+    * @returns The capitalized version of the input string.
+    */
+    static string capitalizeString(const string& txt);
+
+    /**
+    * * Finds the longest word in a sentence (string).
+    *
+    * @param txt The input sentence to find the longest word in.
+    * @returns The longest word in the sentence.
+    */
+    static string largestWordInString(const string& txt);
+
+};
 
 #endif //STRINGS_STRINGFUNCS_H
